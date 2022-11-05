ca071f069e0105d8beb5fc1c4db7de7e7bc776e6
Adrian Omodeo Chaves <aomodeo@gmail.com>
2022-11-05 15:06:05 -0600

    Update settings.py

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--$^z_t2sc^()u=0nw6naer!csx^vomj6k_ylh0f+^xkp6tf+^2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
